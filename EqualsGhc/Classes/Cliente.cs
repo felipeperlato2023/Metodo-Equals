ce6eee8a221f00c467236e8dc0891fb5867a341a
Felipe Perlato <fe.perlato@gmail.com>
2023-11-14 22:51:13 -0300

    primeiro commit

---
+++ EqualsGhc/Classes/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualsGhc.Classes
+{
+    internal class Cliente
+    {
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Cliente))
+            {
+                return false;
+            }
+
+            Cliente outro = obj as Cliente;
+
+
+            return Email.Equals(outro.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+                
+        }
+    }
+}
---
+++ EqualsGhc/Program.cs
@@ -0,0 +1,19 @@
+﻿using EqualsGhc.Classes;
+internal class Program
+ 
+{
+    private static void Main(string[] args)
+    {
+
+        Cliente a = new Cliente {Nome = "Maria", Email = "alex@gmail.com" };
+
+        Cliente b = new Cliente { Nome = "Maria", Email = "alex@gmail.com" };
+
+
+        Console.WriteLine(a.Equals(b));
+        
+        Console.WriteLine(a.GetHashCode());
+
+        Console.WriteLine(b.GetHashCode());
+    }
+}
